8b2842603fc4306d2724cc19f2001fee80c790b1
wanderer <andrey.sn41@gmail.com>
2026-02-17 14:31:55 +0500

    init

--- src/main/kotlin/OrderProcessor.kt
+++ src/main/kotlin/OrderProcessor.kt
@@ -1,16 +1,32 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+fun main() {
+    fun process(p: List<String>, a: Double, d: Boolean) {
+        var t = 0.0
+        for (i in p) {
+            if (i == "Phone") {
+                t += 500.0
+            } else if (i == "Laptop") {
+                t += 1200.0
+            } else {
+                t += 10.0
+            }
+        }
+
+        if (t > 1000) {
+            t = t - (t * 0.1)
+        }
+
+        if (d) {
+            t += 50.0
+        }
+
+        println("Items: " + p)
+        println("Total sum: " + t)
+
+        // Сохранение в "базу данных"
+        println("Saving to DB...")
     }
 }
